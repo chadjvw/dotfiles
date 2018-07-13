1d0507deb75d8873cc141705f49955af79c7b589
cvanwyhe <cvanwyhe@powercosts.com>
2018-07-13 15:41:08 -0500

    Fix fish config and tweak packages

--- templates/dotfiles/link/config/fish/config.fish
+++ templates/dotfiles/link/config/fish/config.fish
@@ -1,1 +1,35 @@
-set -gx PATH /home/{{ username }}/programs /home/{{ username }}/.local $PATH
+{% for file in manual_programs.files %}
+set fish_user_paths $fish_user_paths {{ file.path }}
+{% endfor %}
+set fish_user_paths $fish_user_paths /home/{{ username }}/.local/bin
+
+alias vi="nvim"
+alias vim="nvim"
+alias svim="sudo nvim"
+alias svi="sudo nvim"
+alias edit="nvim"
+
+alias la="ls -alhF"
+alias ..="cd .."
+alias ...="cd ../.."
+
+alias mkdir="mkdir -pv"
+alias mv="mv -v"
+alias rm="rm -vi"
+alias cp="cp -v"
+
+alias update="sudo apt update; sudo apt upgrade -y"
+
+## set some other defaults ##
+alias df="df -H"
+alias du="du -ch"
+
+# top is atop, just like vi is vim
+alias top="htop"
+
+alias netstat="sudo netstat -tulpn"
+
+alias sudo="sudo "
+alias ping="ping -c 5"
+alias cloud="ssh {{ work_username }}@cloud.powercosts.com"
+alias untar="tar -xvf"
--- templates/dotfiles/link/config/nvim/init.vim
+++ templates/dotfiles/link/config/nvim/init.vim
@@ -1,13 +1,12 @@
 set termguicolors
-let g:airline_powerline_fonts = 1
-let g:airline_theme='light'
-
-set tabstop=4
+set tabstop=2
 set shiftwidth=4
 set softtabstop=0 noexpandtab
 
+let g:airline_powerline_fonts = 1
+let g:airline_theme='light'
 
-call plug#begin('~/.local/share/nvim/plugged')
+call plug#begin('/home/{{ username }}/.local/share/nvim/plugged')
 
 Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
