f0db5c342041a629ddb8ac682ecd39748099a550
Chad Van Wyhe <1483653+chadjvw@users.noreply.github.com>
2019-04-16 14:27:11 -0500

    Switch snaps to flatpak, add more config files

--- templates/dotfiles/link/.bashrc
+++ templates/dotfiles/link/.bashrc
@@ -5,3 +5,11 @@ export DOTFILES={{ dotfiles_home }}
 for file in $DOTFILES/source/*; do
   source "$file"
 done
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export EDITOR=nvim
+export AWS_EC2_METADATA_DISABLED=1
+export SXHKD_SHELL=/usr/bin/bash
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_DIRS=$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
--- templates/dotfiles/link/config/fish/config.fish
+++ templates/dotfiles/link/config/fish/config.fish
@@ -5,10 +5,8 @@ set fish_user_paths $fish_user_paths /home/{{ username }}/.dotfiles/source
 set fish_user_paths $fish_user_paths /home/{{ username }}/.local/bin
 set fish_user_paths $fish_user_paths /home/{{ username }}/programs/pcicloud
 set fish_user_paths $fish_user_paths /home/{{ username }}/.cargo/env
-set fish_user_paths $fish_user_paths /snap/bin
 
-# https://github.com/jsit/night-owl-iterm2-theme
-# Set colors for Night Olw
+# Override fish colors
 set fish_color_normal normal
 set fish_color_quote bryellow
 set fish_color_autosuggestion brblack
@@ -22,23 +20,25 @@ set fish_color_param brwhite
 set fish_color_escape red
 set fish_color_redirection magenta
 set fish_color_cwd magenta
-set fish_pager_color_progress brwhite --background=brmagenta
+set fish_pager_color_progress brwhite --background=blue
 set fish_pager_color_completion blue
 set fish_pager_color_description yellow
-set fish_color_match --background=brmagenta
+set fish_color_match brwhite --background=brmagenta
 set fish_color_history_current brgreen
 
 # Colored man pages
 set -g man_blink -o red
 set -g man_bold -o green
-set -g man_standout -b cyan
+set -g man_standout red
 set -g man_underline -u yellow
 
 set -Ux JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
 set -Ux EDITOR nvim
 set -Ux AWS_EC2_METADATA_DISABLED 1
-set -U SXHKD_SHELL /usr/bin/bash
-set -U XDG_CONFIG_HOME "$HOME/.config" 
+set -U SXHKD_SHELL /bin/bash
+set -U XDG_DATA_HOME "$HOME/.local/share"
+set -U XDG_CONFIG_HOME "$HOME/.config"
+set -U XDG_DATA_DIRS "$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
 
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
@@ -67,7 +67,7 @@ alias mv="mv -v"
 alias rm="rm -vi"
 alias cp="cp -v"
 
-alias update="sudo apt update; sudo apt upgrade -y"
+alias update="sudo apt update; sudo apt upgrade -y; flatpak update -y"
 alias df="df -H"
 alias du="du -ch"
 alias top="gotop"
@@ -77,5 +77,4 @@ alias netstat="sudo netstat -tulpn"
 
 alias sudo="sudo "
 alias ping="ping -c 5"
-alias cloud="ssh cvanwyhe@cloud.powercosts.com"
 alias untar="tar -xvf"
