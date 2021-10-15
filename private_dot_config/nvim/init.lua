67477a8e94ca89e169117c657f0773bc23465317
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2021-10-15 01:49:02 -0600

    only use impatient.nvim on mac

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -4,7 +4,10 @@ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-require 'impatient'
+if vim.fn.has('macunix') == 1 then
+  require 'impatient'
+end
+
 require 'plugins'
 require 'options'
 require 'mappings'
