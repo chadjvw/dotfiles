e61f9002edfcd08bd101ce46cc750d2aa929def6
Chad Van Wyhe <1483653+chadjvw@users.noreply.github.com>
2021-12-21 09:34:22 -0700

    fix lua formatting error

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -134,13 +134,11 @@ return packer.startup({
         }
 
         use {
-        "folke/which-key.nvim",
-        config = function()
-          require("which-key").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-          }
+            "folke/which-key.nvim",
+            config = function()
+                require('which-key').setup()
+            end
+        }
     end,
     config = {
         profile = {
