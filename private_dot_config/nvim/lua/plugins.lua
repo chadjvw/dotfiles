6fecd576b4ea7a152867882685127474ab97f87c
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2021-12-21 08:52:09 -0700

    add which-key

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -132,6 +132,15 @@ return packer.startup({
                 require('neoscroll').setup()
             end
         }
+
+        use {
+        "folke/which-key.nvim",
+        config = function()
+          require("which-key").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+          }
     end,
     config = {
         profile = {
