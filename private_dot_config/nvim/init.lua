6c4a4f9fd7ecaaa5bb48b325cef33e3a5c7c3549
Chad Van Wyhe <1483653+chadjvw@users.noreply.github.com>
2022-05-26 09:55:48 -0600

    Switch wilder to lua config, fix initial load issue with packer

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,9 +1,3 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
 if vim.fn.has('macunix') == 1 then
   require 'impatient'
 end
--- private_dot_config/nvim/run_1ExecutePackerSync.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Updating Neovim plugins...'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
