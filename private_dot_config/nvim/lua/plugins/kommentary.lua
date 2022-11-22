6d22e4ba2dfdb8f06b2ec1109a9931f15d186f15
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2022-11-22 16:33:26 -0700

    Switch to doom-nvim over a custom config

--- private_dot_config/nvim/lua/plugins/kommentary.lua
+++
@@ -1,9 +0,0 @@
-local present, kommentary = pcall(require, 'kommentary.config')
-
-if not present then
-   return
-end
-
-kommentary.configure_language("default", {
-    prefer_single_line_comments = true
-})
