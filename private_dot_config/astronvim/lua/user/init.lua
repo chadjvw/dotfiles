ae413885594931739969690998a48c4b87df0fbc
Chad Van Wyhe <github@chad.vanwyhe.xyz>
2022-11-23 13:46:36 -0700

    Switch to AstroNvim, add rust components

---
+++ private_dot_config/astronvim/lua/user/init.lua
@@ -0,0 +1,47 @@
+local config = {
+    colorscheme = "gruvbox",
+    options = {
+        opt = {
+            relativenumber = false -- sets vim.opt.relativenumber
+        }
+    },
+    highlights = {
+        gruvbox = {
+            StatusLine = {
+                fg = "#ebdbb2",
+                bg = "#504945"
+            }
+        }
+    },
+    plugins = {
+        init = {{"ellisonleao/gruvbox.nvim"}},
+        treesitter = {
+            ensure_installed = {"rust", "yaml", "typescript", "javascript", "css", "scss", "java", "json", "bash",
+                                "fish", "html", "kotlin", "markdown", "toml", "sql"}
+        },
+        heirline = function(config)
+            -- the first element of the default configuration table is the statusline
+            config[1] = {
+              -- set the fg/bg of the statusline
+              hl = { fg = "fg", bg = "bg" },
+              -- when adding the mode component, enable the mode text with padding to the left/right of it
+              astronvim.status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
+              -- add all the other components for the statusline
+              astronvim.status.component.git_branch(),
+              astronvim.status.component.file_info(),
+              astronvim.status.component.git_diff(),
+              astronvim.status.component.diagnostics(),
+              astronvim.status.component.fill(),
+              astronvim.status.component.macro_recording(),
+              astronvim.status.component.fill(),
+              astronvim.status.component.lsp(),
+              astronvim.status.component.treesitter(),
+              astronvim.status.component.nav(),
+            }
+            -- return the final configuration table
+            return config
+          end,
+    }
+}
+
+return config
