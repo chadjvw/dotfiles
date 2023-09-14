e7c42d81cd466c6048c8abb27d635403f926d746
Chad Van Wyhe <1483653+chadjvw@users.noreply.github.com>
2023-09-14 08:33:29 -0600

    Add more everforest

--- private_dot_config/nvim/lua/custom/chadrc.lua
+++ private_dot_config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "gruvbox_light" },
+  theme = "everforest",
+  -- theme_toggle = { "gruvbox", "gruvbox_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
